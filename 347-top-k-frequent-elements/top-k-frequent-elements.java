a0e1e258fdde279851d58a1295685c56c8578983
Aakash Ansh <89316946+Ansh0021@users.noreply.github.com>
2024-10-03 15:11:15 +0530

    Time: 15 ms (38.29%) | Memory: 48.3 MB (71.76%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -2,31 +2,29 @@ import java.util.*;
 
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        // Step 1: Count frequencies using a HashMap
         HashMap<Integer, Integer> hm = new HashMap<>();
         for (int x : nums) {
-            hm.put(x, hm.getOrDefault(x, 0) + 1);
+            if(!hm.containsKey(x)){
+                hm.put(x,1);
+            }
+            else hm.put(x,hm.get(x)+1);
         }
-
-        // Step 2: Use a PriorityQueue (min-heap) to keep the top k frequent elements
         PriorityQueue<Map.Entry<Integer, Integer>> heap = new PriorityQueue<>(
-            (a, b) -> a.getValue() - b.getValue() // min-heap based on frequency
+            (a, b) -> a.getValue() - b.getValue() 
         );
 
-        // Step 3: Add entries to the heap, maintaining the size of the heap to k
         for (Map.Entry<Integer, Integer> entry : hm.entrySet()) {
             heap.offer(entry);
             if (heap.size() > k) {
-                heap.poll(); // remove the least frequent element
+                heap.poll(); 
             }
         }
 
-        // Step 4: Extract the top k elements from the heap and store them in an array
-        int[] result = new int[k];
+        int[] res = new int[k];
         for (int i = k - 1; i >= 0; i--) {
-            result[i] = heap.poll().getKey();
+            res[i] = heap.poll().getKey();
         }
 
-        return result;
+        return res;
     }
 }
