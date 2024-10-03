a6b5054964ab98db8edf9f517e53ee036c3bc9d7
Aakash Ansh <89316946+Ansh0021@users.noreply.github.com>
2024-10-03 19:47:55 +0530

    Time: 15 ms (26.62%) | Memory: 44.6 MB (38.80%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -4,17 +4,19 @@ class Solution {
         HashMap<Character,Integer> hm2=new HashMap<>();
         for(int i=0;i<s.length();i++){
             char x=s.charAt(i);
-            if(hm1.containsKey(x)){
-                hm1.put(x,hm1.get(x)+1);
-            }
-            else hm1.put(x,1);
+            hm1.put(x, hm1.getOrDefault(x, 0) + 1);
+            // if(hm1.containsKey(x)){
+            //     hm1.put(x,hm1.get(x)+1);
+            // }
+            // else hm1.put(x,1);
         }
            for(int i=0;i<t.length();i++){
             char x=t.charAt(i);
-            if(hm2.containsKey(x)){
-                hm2.put(x,hm2.get(x)+1);
-            }
-            else hm2.put(x,1);
+            hm2.put(x, hm2.getOrDefault(x, 0) + 1);
+            // if(hm2.containsKey(x)){
+            //     hm2.put(x,hm2.get(x)+1);
+            // }
+            // else hm2.put(x,1);
         }
         if(hm1.equals(hm2)) return true;
         else return false;
